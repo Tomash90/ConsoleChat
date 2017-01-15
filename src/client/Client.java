e95f6b52f1433f5fbd6c50764907a18cb297061b
Tomasz <tomasz.sulek90@gmail.com>
2017-01-15 12:59:06 +0100

    Update Client.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -10,11 +10,10 @@ import java.util.Scanner;
 public class Client {
 
 	public static void main(String[] args) {
-		Socket socket = null;
 		int port = 5000;
 		try {
 			while (true) {
-				socket = new Socket("192.168.0.102", port);
+				Socket socket = new Socket("192.168.0.102", port);
 				OutputStream outToServer = socket.getOutputStream();
 				DataOutputStream out = new DataOutputStream(outToServer);
 				Scanner scanner = new Scanner(System.in);
